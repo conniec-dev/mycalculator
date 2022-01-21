7e5fadc68cce5bc3ef7531ab07f266683d328cf2
Connie <conniecanelon97@gmail.com>
2022-01-21 12:06:51 -0500

    Changing names again

--- calculator.go
+++ calculator.go
@@ -2,6 +2,7 @@ package mycalculator
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"strconv"
@@ -12,24 +13,21 @@ type Calc struct{}
 
 func (Calc) Operate(input string, operation string) (int, error) {
 	cleanInput := strings.Split(input, operation)
-	first, err := c.parseString(cleanInput[0])
-	if err != nil {
-		return 0, err
-	}
-	second, err := c.parseString(cleanInput[1])
-	if err != nil {
-		return 0, err
-	}
-
+	operator1 := parseString(cleanInput[0])
+	operator2 := parseString(cleanInput[1])
 	switch operation {
 	case "+":
-		return first + second, nil
+		fmt.Println(operator1 + operator2)
+		return operator1 + operator2
 	case "-":
-		return first - second, nil
+		fmt.Println(operator1 - operator2)
+		return operator1 - operator2
 	case "*":
-		return first * second, nil
+		fmt.Println(operator1 * operator2)
+		return operator1 * operator2
 	case "/":
-		return first / second, nil
+		fmt.Println(operator1 / operator2)
+		return operator1 / operator2
 	default:
 		log.Println(operation, "operation is not supported!")
 		return 0, nil
@@ -37,7 +35,7 @@ func (Calc) Operate(input string, operation string) (int, error) {
 	}
 }
 
-func (calc) parseString(operator string) (int, error) {
+func (Calc) parseString(operator string) (int, error) {
 	result, err := strconv.Atoi(operator)
 	return result, err
 }
